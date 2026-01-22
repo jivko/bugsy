3cdf9bfcff79a70187617757efd1e17423ad7541
Jivko Georgiev <jivkobg@gmail.com>
2026-01-22 15:52:20 +0200

    Add Users and Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :session
+  resource :registration, only: [ :new, :create ]
+  resources :passwords, param: :token
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
