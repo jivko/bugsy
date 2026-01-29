43b93322f1107950ade669c2a35915b3d6fc3b3f
Jivko Georgiev <jivkobg@gmail.com>
2026-01-29 15:06:00 +0200

    Add roles to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  enum :role, { member: 0, admin: 1 }, default: :member
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
